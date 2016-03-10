32e6e1464c7ed4f5effb25e11ab803b4ac82286d
Colin Anderson <andercol.4@gmail.com>
2016-03-09 22:57:56 -0700

    basic bot working

--- realtime_sample.rb
+++ realtime_sample.rb
@@ -1,5 +1,6 @@
 require 'slack-ruby-client'
 require 'dotenv'
+require 'pry'
 Dotenv.load
 
 Slack.configure do |config|
@@ -9,5 +10,9 @@ end
 client = Slack::RealTime::Client.new
 client.on :message do |data|
   puts data
+  case data.text
+  when 'bot hi'
+    client.message channel: data.channel, text: "Hi <@#{data.user}>!"
+  end
 end
 client.start!
